8271864bbefd9ad167e46dcc7fcd3c5dc498595c
imakado-kenji <imakado-kenji@389d678b-af27-0410-90e4-ce99a0ceb742>
2009-05-28 12:39:06 +0000

    added WebService::SimpleAPI::Wikipedia to extlib
    git-svn-id: https://code.kayac.com/svn/bm11/inamena/trunk@13638 389d678b-af27-0410-90e4-ce99a0ceb742

--- lib/Inamena/Controller/Root.pm
+++ lib/Inamena/Controller/Root.pm
@@ -6,21 +6,23 @@ has '+namespace' => default => '';
 # default 404 handler
 sub default :Path :Args {
     my ($self, $c) = @_;
+
     $c->res->status(404);
+    $c->res->body('404 Not Found');
     $c->view('MT')->template('errors/404');
+    
 }
 
 sub index :Path :Args(0) {
     my ($self, $c) = @_;
+    $c->res->body('Ark Default Index');
 }
 
 sub end :Private {
     my ($self, $c) = @_;
 
-    $c->res->header('Cache-Control' => 'private');
-
     unless ($c->res->body or $c->res->status =~ /^3\d\d/) {
-        $c->forward( $c->view('MT') );
+        $c->view('MT')->template('');
     }
 }
 
--- tools/test_format_junit.pl
+++ tools/test_format_junit.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
+use lib "$FindBin::RealBin/../extlib";
+
 
 use TAP::Harness::JUnit;
 my $harness = TAP::Harness::JUnit->new(
